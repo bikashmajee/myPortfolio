a6464c689b2454c29caebecfa95c1ad0dae803e9
Bikash Majee <majee.bikash1998@gmail.com>
2023-02-10 18:56:32 +0530

    first commit with Navbar

---
+++ src/component/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../common/navbar'
+import Footer from '../common/footer'
+
+function index() {
+  return (
+    <>
+    <Navbar/>
+
+    {/* <div>Welcome to my Portfolio</div> */}
+    {/* <Footer/> */}
+
+    </>
+  )
+}
+
+export default index
